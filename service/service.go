08a08c1036fe0458835162f29ba78cdb1f1020bc
gunger <geoffunger@gmail.com>
2024-11-24 21:34:56 -0800

    Added logging to getScheduledTimes func

--- service/service.go
+++ service/service.go
@@ -11,6 +11,7 @@ import (
 
 type Service struct {
 	pb.UnimplementedTransitPDXServer
+	serviceLogger *log.Logger
 }
 
 func New() *Service {
@@ -18,6 +19,7 @@ func New() *Service {
 }
 
 func (service *Service) Start(logger *log.Logger) error {
+	service.serviceLogger = logger
 	logger.Println("starting service")
 	lis, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
@@ -31,6 +33,7 @@ func (service *Service) Start(logger *log.Logger) error {
 }
 
 func (s *Service) GetScheduledTimes(context.Context, *pb.StopId) (*pb.NextScheduledTimes, error) {
+	s.serviceLogger.Print("GetScheduledTimes called")
 	times := []int32{123, 432}
 	return &pb.NextScheduledTimes{
 		ScheduledTimes: times,
